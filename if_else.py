06ce4666eff2be0aa2ebe32374c8d9a6fc4ef583
khusanovich <asliddinergashev@mail.ru>
2021-12-03 18:36:57 +0100

    if_else darsi kodlari yuklandi

---
+++ if_else.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 17:31:08 2021
+
+@author: khusanovich
+"""
+
+# autos = ['audi', 'bmw', 'mercedes', "vw", 'porsche']
+# for auto in autos:
+#     if auto == 'bmw' or auto == 'vw':
+#         print(auto.upper())
+#     else:
+#         print(auto.title())
+        
+# ism = "Ali"
+# ism.lower() == 'ali'
+
+# ism = input('Ismingiz nima ? >>>')
+# if ism.lower() != 'ali':
+#     print(f"Uzr {ism.title()}, biz Alini kutyapmiz.")
+# else:
+#     print("Salom, Ali")
+
+# javob = float(input("12x6 nechiga teng?>>>"))
+# if javob!=72:
+#     print("Javob xato!")
+# else:
+#     print("Javob to'g'ri, tabriklaymiz!")
+    
+# son = float(input("5x5  nechchi?"))
+# if son == 25:
+#     print("Tabriklaymiz, javob to'g'ri!")
+# else:
+#     print("Javob xato, kallani ishlating.")
+
+# yosh = int(input("Yoshingiz nechida?"))
+# if yosh < 18:
+#     print("Sizga kirish mumkin emas, 18 dan oshganingizdan keyin keling.")
+# else:
+#     print("Marhamat kiring")
+
+# yil = int(input("Tug'ilgan yilingizni kiriting>>>"))
+# if 2021-yil < 18:
+#     print(f"Yoshingiz {2021-yil} da ekan.")
+#     print("Kirish mumkin emas.")
+# else:
+#     print("Kiravering.")
+
+# ## Yangi cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia'] degan ro'yxat tuzing, ro'yxat elementlarining birinchi harfini katta qilib konsolga chqaring. GM uchun ikkala harfni katta qiling.
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car == 'gm':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+        
+# ## Yuqoridagi mashqni teng emas (!=) operatori yordamida bajaring.
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car != 'gm':
+#         print(car.title())
+#     else:
+#         print(car.upper())
+
+# ## Foydalanuvchi login ismini so'rang. Agar login admin bo'lsa, "Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?" xabarini konsolga chiqaring. Aks holda, "Xush kelibsiz, {foydalanuvchi_ismi}!" matnini konsolga chiqaring.
+# login = input("Loginingizni kiriting:")
+# if login == 'admin':
+#     print(f"Xush kelibsiz {login}. Foydalanuvchilar ro'yxatini ko'rasizmi?")
+# else:
+#     print(f"Xush kelibsiz, {login.title()}!")
+
+# ## Foydalanuvchidan 2 ta son kiritishni so'rang. Agar ikki son bir-biriga teng bo'lsa, "Sonlar teng" ekan degan yozuvni konsolga chiqaring.
+# print("Ikkita son kiriting:")
+# son1 =  float(input("Birinchi son: "))   
+# son2 = float(input("Ikkinchi son: "))
+# if son1 == son2:
+#     print("Sonlar teng ekan.")
+# else:
+#     print("Sonlar teng emas.")
+
+# ## Foydalanuvchidan istalgan son kiritishni so'rang. Agar son manfiy bo'lsa konsolga "Manfiy son", agar musbat bo'lsa "Musbat son" degan xabarni chiqaring
+# print("Biron son kiriting>>>")
+# son = float(input("son:"))
+# if son < 0:
+#     print("Manfiy son")
+# else:
+#     print("Musbat son")
+    
+# ## Foydalanuvchidan son kiritishni so'rang, agar son musbat bo'lsa uning ildizini hisoblab konsolga chiqaring. Agar son manfiy bo'lsa, "Musbat son kiriting" degan xabarni chiqaring
+# print("Biron son kiriting.")
+# son = float(input("son:"))
+# if son > 0:
+#     print(son**0.5)
+# else:
+#     print("Musbat son kiriting!")      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
